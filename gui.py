593d8be91725a05195b12627a8f8ae9b89a1088c
Ananya M S <62280251+ananyams123@users.noreply.github.com>
2021-03-25 13:02:06 +0530

    Update gui.py
    modified

--- gui.py
+++ gui.py
@@ -15,3 +15,24 @@ green = (0,200,0)
 bright_red = (255,0,0)
 bright_green = (0,255,0)
 
+
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption('GUI Speech Recognition')
+
+gameDisplay.fill(white)
+carImg = pygame.image.load('img.jpg')
+gameDisplay.blit(carImg,(0,0))
+
+def close():
+    pygame.quit()
+    quit()
+
+def message_display(text):
+    largeText = pygame.font.Font('freesansbold.ttf',30)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((display_width/2),(display_height/2))
+    gameDisplay.blit(TextSurf, TextRect)
+
+    pygame.display.update()
+    
+
