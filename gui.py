43330cf3a47cb31aa23b53d03c27395d5e1eae20
anoushkaanand <54367017+anoushkaanand@users.noreply.github.com>
2021-03-25 13:00:48 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -6,3 +6,12 @@ pygame.init()
 
 display_width = 800
 display_height = 600
+
+black = (0,0,0)
+alpha = (0,88,255)
+white = (255,255,255)
+red = (200,0,0)
+green = (0,200,0)
+bright_red = (255,0,0)
+bright_green = (0,255,0)
+
